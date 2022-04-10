a5612bcb7c60570c1e7f677c1453b5ec74e5d932
Taras <taras.khvyl@gmail.com>
2022-04-10 18:09:50 +0300

    WIP: added product card

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {
-  createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword,
+  signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
