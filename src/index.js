23cfd4aa6590b06e9a6c777d8b3654766314e60e
Taras <taras.khvyl@gmail.com>
2022-04-20 09:29:09 +0300

    WIP: added Stripe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
@@ -7,13 +8,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { persistor, store } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <App/>
+          <Elements stripe={stripePromise}>
+            <App/>
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles";
 
@@ -34,6 +35,7 @@ const Checkout = () => {
         })
       }
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm/>
     </CheckoutContainer>
   );
 };
